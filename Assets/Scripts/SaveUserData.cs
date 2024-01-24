8a4a65d56dd1a785baa30fa837bc2046e245838a
Théo Lefeuvre <theo.lefeuvre@hotmail.fr>
2024-01-24 17:24:34 +0100

    save data + break hand velocity

---
+++ Assets/Scripts/SaveUserData.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class SaveUserData : MonoBehaviour
+{
+    StringBuilder newuserdata = new System.Text.StringBuilder();
+
+
+    // ecrit les donnees des variables dans le StringBuilder newuserdata puis ecrit les données du StringBuilder newuserdata dans le fichier UsersData.csv 
+
+    public void WriteNewUserData() // (float v, int t) // variables mises en exemple
+    {
+        
+        var path = Path.Combine("C:\\Users\\etudiant\\Documents", "UsersData.csv");
+        Debug.Log(path);
+        FindUsersDataFile(path);
+
+        NewUserData(); // (v, t); // variables mises en exemple
+
+        SaveNewUserData(path, newuserdata.ToString());
+    }
+
+
+    // verifie si le fichier existe, sinon lance la fonction CreateUsersDataFile()
+
+    void FindUsersDataFile(string path)
+    {
+        if (!File.Exists(path))
+        {
+            CreateUsersDataFile();
+        }
+        else
+        {
+            ReadUsersDataFile(path);
+        }
+    }
+
+
+    // cree les headers du nouveau fichier et les ajoute au StringBuildder newuserdata
+
+    void CreateUsersDataFile()
+    {
+        /* "Velocite" + ";" + "Temps" + ";" // variables mises en exemple*/
+        string headers =  "Genre" +";"+ "Hand Type" + ";"+  "Totale Time" + ";" + " Round_1 Fruit" + ";" + " Round_2 Fruit" + ";" + " Round_3 Fruit" + ";" + " Round_4 Fruit" + ";" + " Round_5 Fruit" + ";" + " Round_1 Time" + ";" + " Round_2 Time" + ";" + " Round_3 Time" + ";" + " Round_4 Time" + ";" + " Round_5 Time" + ";"; 
+
+        newuserdata.AppendLine(headers);
+    }
+
+
+    // si pas encore recuperees, recupere les anciennes donnees du fichier et les enregistre dans le StreamBuilder newuserdata
+
+    void ReadUsersDataFile(string path)
+    {
+        if (newuserdata.ToString() == "")
+        {
+            string data = File.ReadAllText(path, Encoding.UTF8);
+
+            newuserdata.Append(data);
+        }
+    }
+
+    // ajoute les données des variables au StringBuilder newuserdata
+
+    void NewUserData() // (float v, int t) // variables mises en exemple
+    {
+        string data =  "" + ";" + "Pince" + ";"+ "20" + ";" + "1" + ";" + "2" + ";" + "3" + ";" + "3" + ";" + "2" + ";" + "15" + ";" + "17" + ";" + "12" + ";" + "13" + ";" + "25" + ";" ;
+
+        newuserdata.AppendLine(data);
+    }
+
+
+    // ecrit les données du StringBuilder newuserdata dans le fichier UsersData.csv 
+
+    void SaveNewUserData(string path, string data)
+    {
+        using (var writer = new StreamWriter(path))
+        {
+            writer.Write(data, Encoding.UTF8);
+            writer.Flush();
+            writer.Close();
+        }
+    }
+
+    private void Start()
+    {
+        WriteNewUserData();
+    }
+}
