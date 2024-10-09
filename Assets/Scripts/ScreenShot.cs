37731c76a0335f13dfa3dc361573193e323b1a3b
TitouanLEFROU <79263135+TitouanLEFROU@users.noreply.github.com>
2024-10-09 15:20:10 +0200

    backup

---
+++ Assets/Scripts/ScreenShot.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour
+{
+   
+    [SerializeField]
+    private string path;
+    private string fileName;
+    [SerializeField]
+    [Range(1, 5)]
+    private int size = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("SCREENSHOT");
+            fileName = "screenshot ";
+            fileName += System.Guid.NewGuid().ToString() + ".png";
+
+            ScreenCapture.CaptureScreenshot(path + fileName, size);
+        }
+    }
+}
+
+
+
